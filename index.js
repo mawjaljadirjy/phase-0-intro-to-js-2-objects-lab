29c660c4e279c056ca83a2ac88880a7bc058c310
Mawj_Aljadirjy <eng_mawj_aljadirjy@yahoo.com>
2024-03-01 20:12:45 +0300

    Completed assiginment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Mawj Sam' 
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   
+    const updatedEmployee = Object.assign({}, employee);
+
+    updatedEmployee[key] = value;
+    
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   
+    employee[key] = value;
+    return employee;
+}
+function  deleteFromEmployeeByKey(employee, key){
+    
+      const updatedEmployee = Object.assign({}, employee);
+
+      delete updatedEmployee[key];
+
+      return updatedEmployee;
+ 
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
